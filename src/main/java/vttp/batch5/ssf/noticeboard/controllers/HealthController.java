84dccb08a7f4be1cbf12806674ef4f8f5c916c7f
kennykankush <155627682+kennykankush@users.noreply.github.com>
2024-12-13 16:03:40 +0800

    503

--- src/main/java/vttp/batch5/ssf/noticeboard/controllers/HealthController.java
+++ src/main/java/vttp/batch5/ssf/noticeboard/controllers/HealthController.java
@@ -3,9 +3,7 @@ package vttp.batch5.ssf.noticeboard.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,7 +34,7 @@ public class HealthController {
         }   catch (Exception e){
 
         return ResponseEntity
-                .status(400)
+                .status(503)
                 .contentType(MediaType.APPLICATION_JSON)
                 .body("{}");
 
